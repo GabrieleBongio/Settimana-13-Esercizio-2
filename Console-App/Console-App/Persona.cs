a6e03e1b40d02eb8fb1ae0fbfa18fd06b3985df1
Gabriele Bongio <bongiodieci@gmail.com>
2024-01-30 14:45:36 +0100

    Esercizio Finito

---
+++ Console-App/Console-App/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Età { get; set; }
+
+        public string GetNome() { return Nome; }
+        public string GetCognome() { return Cognome; }
+        public int GetEtà() { return Età; }
+        public string GetDettagli() { return $"{Nome} {Cognome} di {Età} anni"; }
+
+        public Persona() { }
+
+        public Persona(string nome, string cognome, int età)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Età = età;
+        }
+
+
+    }
+}
---
+++ Console-App/Console-App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Gianni", "Morandi", 79);
+            Console.WriteLine(persona1.GetNome());
+            Console.WriteLine(persona1.GetCognome());
+            Console.WriteLine(persona1.GetEtà());
+
+            Console.WriteLine(persona1.GetDettagli());
+            Console.WriteLine("");
+
+            Persona persona2 = new Persona();
+            Console.WriteLine("Inserisci il nome:");
+            persona2.Nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il cognome:");
+            persona2.Cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci l'età:");
+            persona2.Età = Convert.ToInt16(Console.ReadLine());
+            
+            Console.WriteLine(persona2.GetDettagli());
+
+            Console.ReadLine();
+        }
+    }
+}
